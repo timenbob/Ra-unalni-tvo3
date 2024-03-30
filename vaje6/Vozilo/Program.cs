171fd2023a6340d5a761b234336cbb34b8763583
timenbob <timen.bobnar@gmail.com>
2024-03-30 18:12:57 +0100

    vaje6

---
+++ vaje6/Vozilo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vozilo
+{
+    
+
+    public class Vozilo
+    {
+        //podatki
+        private double gorivo;
+        private double kapaciteta;
+        private double poraba;
+
+        //konstruktor
+        public Vozilo(double kapaciteta, double poraba)
+        {
+            if (kapaciteta <= 0)
+            {
+                throw new ArgumentException("Kapaciteta ne sme biti negativna");
+            }
+            if (poraba <= 0)
+            {
+                throw new ArgumentException("Poraba ne sme biti negativna");
+
+            }
+            else
+            {
+                this.kapaciteta = kapaciteta;
+                this.poraba = poraba;
+                this.gorivo = kapaciteta;
+            }
+            
+        }
+        //lastnost get neki mi naredimo set nastavi
+        public double PreostaliKilometri
+        {
+            get
+            {
+                return gorivo / poraba * 100;
+            }
+        }
+
+        //metoda
+        public void Crpalka()
+        {
+            gorivo = kapaciteta;
+        }
+
+        public Boolean AliPrevozi(double[] pot)
+        {
+            foreach (var el in pot)
+            {
+                if (el == 0)
+                {
+                    Crpalka();
+                }
+                if (el < 0 )
+                {
+                    throw new Exception("narobe pot");
+                }
+                if (el > PreostaliKilometri)
+                {
+                    return false;
+                }
+                gorivo -= el * poraba;
+            }
+            return true;
+        }
+    }
+}
---
+++ vaje6/VoziloTests/VoziloTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vozilo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vozilo.Tests
+{
+    [TestClass()]
+    public class VoziloTests
+    {
+        [TestMethod()]
+        public void VoziloTest1()
+        {
+            double kapaciteta = 50;
+            double poraba = 5;
+            Vozilo vozilo = new Vozilo(kapaciteta, poraba);
+            Assert.AreEqual(kapaciteta, vozilo.PreostaliKilometri);
+        }
+
+        [TestMethod()]
+        public void CrpalkaTest1()
+        {
+
+            double kapaciteta = 50;
+            double poraba = 5;
+            Vozilo vozilo = new Vozilo(kapaciteta, poraba);
+            vozilo.Crpalka();
+
+            Assert.AreEqual(kapaciteta, vozilo.PreostaliKilometri);
+        }
+
+        [TestMethod()]
+        public void AliPrevoziTest1()
+        {
+            double kapaciteta = 50;
+            double poraba = 5;
+            double[] pot = { 10, 20, 5 };
+            Vozilo vozilo = new Vozilo(kapaciteta, poraba);
+
+            bool uspesno = vozilo.AliPrevozi(pot);
+
+            Assert.IsTrue(uspesno);
+        }
+
+        [TestMethod()]
+        public void AliPrevoziTest2()
+        {
+            double kapaciteta = 50;
+            double poraba = 5;
+            double[] pot = { 10, 20, 35 };
+            Vozilo vozilo = new Vozilo(kapaciteta, poraba);
+
+            bool uspesno = vozilo.AliPrevozi(pot);
+
+            Assert.IsFalse(uspesno);
+        }
+
+
+    }
+}
