ac0751698804f2848148e011264879da6e4d30c3
timenbob <timen.bobnar@gmail.com>
2024-05-02 20:46:27 +0200

    dn

---
+++ vaje7/ploscadPlus/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ploscadPlus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gumb = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(22, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // gumb
+            // 
+            this.gumb.Location = new System.Drawing.Point(331, 259);
+            this.gumb.Name = "gumb";
+            this.gumb.Size = new System.Drawing.Size(97, 42);
+            this.gumb.TabIndex = 2;
+            this.gumb.Text = "Gibanje";
+            this.gumb.UseVisualStyleBackColor = true;
+            this.gumb.Click += new System.EventHandler(this.gumb_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "hitrost";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gumb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button gumb;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ vaje7/semaforPlusPlus/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace semaforPlusPlus
+{
+    public partial class Form1 : Form
+    {
+        int stevec = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rdeca_Click(object sender, EventArgs e)
+        {
+            this.semafor.BackColor = Color.Red;
+            stevec = 0;
+            this.semafor.Text = "" + stevec;
+        }
+
+        private void Rumena_Click(object sender, EventArgs e)
+        {
+            this.semafor.BackColor = Color.Yellow;
+        }
+
+        private void Zelena_Click(object sender, EventArgs e)
+        {
+            this.semafor.BackColor = Color.Green;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            stevec += 1;
+            this.semafor.Text = ""+ stevec;
+        }
+    }
+}
