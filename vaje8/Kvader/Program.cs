f1ff954c6f2bb51a4d6c1593ebeca4540c13e136
timenbob <timen.bobnar@gmail.com>
2024-05-08 11:03:10 +0200

    hewfuzw

---
+++ vaje8/Kvader/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kvader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Kvader : IComparable<Kvader>
+    {
+        private double dolzina;
+        private double sirina;
+        private double visina;
+
+        // Konstruktor
+        public Kvader(double dolzina, double sirina, double visina)
+        {
+            this.dolzina = dolzina;
+            this.sirina = sirina;
+            this.visina = visina;
+        }
+
+        // Lastnosti
+        public double Dolzina
+        {
+            get { return dolzina; }
+            set { dolzina = value; }
+        }
+
+        public double Sirina
+        {
+            get { return sirina; }
+            set { sirina = value; }
+        }
+
+        public double Visina
+        {
+            get { return visina; }
+            set { visina = value; }
+        }
+
+        // Urejanje glede na volumen kvadra
+        public int CompareTo(Kvader other)
+        {
+            double thisVolume = dolzina * sirina * visina;
+            double otherVolume = other.dolzina * other.sirina * other.visina;
+            return thisVolume.CompareTo(otherVolume);
+        }
+
+        // Relacijski operatorji
+        public static bool operator ==(Kvader a, Kvader b)
+        {
+            return a.dolzina == b.dolzina && a.sirina == b.sirina && a.visina == b.visina;
+        }
+
+        public static bool operator !=(Kvader a, Kvader b)
+        {
+            return !(a == b);
+        }
+
+        // Pomožna metoda za preoblikovanje kvadra v niz
+        public override string ToString()
+        {
+            return $"Dolžina: {dolzina}, Širina: {sirina}, Višina: {visina}";
+        }
+    }
+
+}
---
+++ vaje8/Vektor2D/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektor2D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Vektor2D : IComparable<Vektor2D>
+    {
+        private double x;
+        private double y;
+
+        // Konstruktor
+        public Vektor2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Lastnosti
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        // Urejanje glede na dolžino vektorja
+        public int CompareTo(Vektor2D other)
+        {
+            double thisLength = Math.Sqrt(x * x + y * y);
+            double otherLength = Math.Sqrt(other.x * other.x + other.y * other.y);
+            return thisLength.CompareTo(otherLength);
+        }
+
+        // Relacijski operatorji
+        public static bool operator ==(Vektor2D a, Vektor2D b)
+        {
+            return a.x == b.x && a.y == b.y;
+        }
+
+        public static bool operator !=(Vektor2D a, Vektor2D b)
+        {
+            return !(a == b);
+        }
+
+        // Aritmetični operatorji
+        public static Vektor2D operator +(Vektor2D a, Vektor2D b)
+        {
+            return new Vektor2D(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vektor2D operator -(Vektor2D a, Vektor2D b)
+        {
+            return new Vektor2D(a.x - b.x, a.y - b.y);
+        }
+
+        // Množenje z naravnim številom
+        public static Vektor2D operator *(Vektor2D a, int b)
+        {
+            return new Vektor2D(a.x * b, a.y * b);
+        }
+
+        // Pomožna metoda za preoblikovanje vektorja v niz
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+
+}
