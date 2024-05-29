8f2ce884958e8d1509aa3fea94ae7534754c6fb7
timenbob <timen.bobnar@gmail.com>
2024-05-29 13:26:37 +0200

    bla

---
+++ ucenje/GeomLiki/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeomLiki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public interface IGeomLik
+    {
+        double Ploscina();
+        double Obseg();
+    }
+
+    public class Krog : IGeomLik, IComparable<Krog>
+    {
+        private int _r;
+        private int _Sx;
+        private int _Sy;
+
+        public int Polmer
+        {
+            get { return this._r; }
+            set {
+                if (value < 0) { throw new ArgumentException("radij more bit večji od 0"); }
+
+                this._r = value; }
+        }
+
+        public int Sx
+        {
+            get { return this._Sx; }
+            set { this._Sx = value; } 
+        }
+        public int Sy
+        {
+            get { return this.Sy; }
+            set { this._Sy = value; }   
+        }
+
+        public Krog(int polmer, int sx, int sy)
+        {
+            Polmer = polmer;
+            Sx = sx;
+            Sy = sy;
+        }
+
+        public double Ploscina()
+        {
+            return Math.PI * _r * _r;
+        }
+
+        public double Obseg()
+        {
+            return 2 * Math.PI * _r;
+        }
+        public int CompareTo(Krog other)
+        {
+            return this.Ploscina().CompareTo(other.Ploscina());
+        }
+
+    }
+}
---
+++ ucenje/datoteke/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datoteke
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //hocemo odpret in neki vzet vn
+
+            String povezavaLink = @"Data Source = C:\Users\HP\source\repos\Programiranje_3\ucenje\datoteke\filmi (1).sqlite";
+            SQLiteConnection povezava = new SQLiteConnection(povezavaLink);
+
+            povezava.Open();
+
+            SQLiteCommand ukaz = new SQLiteCommand();
+            ukaz.Connection= povezava;
+            ukaz.CommandType = System.Data.CommandType.Text;
+            ukaz.CommandText = "SELECT * FROM filmi";
+
+            SQLiteDataReader rez = ukaz.ExecuteReader();
+
+
+            while (rez.Read())
+            {
+                for (int i = 0; i < rez.FieldCount; i++)
+                {
+                    Console.Write(rez.GetValue(i) + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            povezava.Close();
+        }
+    }
+}
