27aeaad93759c61c55efba69ee601bf3f7135e98
timenbob <timen.bobnar@gmail.com>
2024-05-29 14:51:52 +0200

    iz

---
+++ TimenBobnar_2Izpit_2023_24/N2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public interface IGeomLik
+        {
+            double Ploscina();
+            double Obseg();
+        }
+
+    }
+}
